e78d77ddb0a6db2d4d6e2f655e2bea2873198bb8
Itsrajsk <rajkashid146303@gmail.com>
2025-09-04 07:35:57 +0530

    Project Completed

--- src/Routers/CustomerRouters.jsx
+++ src/Routers/CustomerRouters.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../customer/pages/HomePage/HomePage";
-import Cart from "../customer/components/Cart/cart";
+import Cart from "../customer/components/Cart/Cart";
 import Navigation from "../customer/components/Navigation/Navigation";
 import Footer from "../customer/components/Footer/Footer";
 import Product from "../customer/components/Product/Product";
