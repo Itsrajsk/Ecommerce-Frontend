2ba87c65be4e3a3ac1851281498580f3f6f97953
Itsrajsk <rajkashid146303@gmail.com>
2025-09-04 07:44:49 +0530

    Project Completed

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://ecommerce-backend-production-3ebf.up.railway.app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
