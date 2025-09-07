0625184bf62c2ab639414f45b5f76036ea859a04
Itsrajsk <rajkashid146303@gmail.com>
2025-09-07 10:27:56 +0530

    Orders Table Completed

--- src/State/store.js
+++ src/State/store.js
@@ -5,6 +5,7 @@ import { customerProductReducer } from "./Product/Reducer";
 import { cartReducer } from "./Cart/Reducer";
 import { orderReducer } from "./Order/Reducer";
 import { paymentReducer } from "./Payment/Reducer";
+import adminOrderReducer from "./Admin/Orders/Reducer";
 
 const rootReducers = combineReducers({
   auth: authReducer,
@@ -12,6 +13,7 @@ const rootReducers = combineReducers({
   cart: cartReducer,
   order: orderReducer,
   payment: paymentReducer,
+  adminOrder: adminOrderReducer,
 });
 
 export const store = legacy_createStore(rootReducers, applyMiddleware(thunk));
